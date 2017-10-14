3151be8b78da7d50a9236ec124c9062b45a6d2aa
Eli Dupree <vcs@elidupree.com>
2017-10-14 13:43:34 -0400

    More prototyping the glue layer

--- codecophony-render-gui/lib/index.js
+++ codecophony-render-gui/lib/index.js
@@ -1,6 +1,6 @@
 var addon = require('../native');
 
-module exports = {
-  set_playback_range = addon.set_playback_range,
-  poll_rendered = addon.poll_rendered,
+module.exports = {
+  set_playback_range: addon.set_playback_range,
+  poll_rendered: addon.poll_rendered,
 }
